2cf6289c090a4ef0e7a06c2a0b7d0e5b7ed78291
Kacper <kacpkuc@gmail.com>
2023-11-09 01:14:00 +0100

    Repositories clean up

--- src/main/java/com/example/SpringSecurityDemo/repo/RoleRepo.java
+++ src/main/java/com/example/SpringSecurityDemo/repo/RoleRepo.java
@@ -4,8 +4,5 @@ import com.example.SpringSecurityDemo.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepo extends JpaRepository<Role, Long> {
-
-
     Role findByName(String name);
-
 }
--- src/main/java/com/example/SpringSecurityDemo/repo/UserRepo.java
+++ src/main/java/com/example/SpringSecurityDemo/repo/UserRepo.java
@@ -4,7 +4,5 @@ import com.example.SpringSecurityDemo.domain.UserMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<UserMember, Long> {
-
     UserMember findByUsername(String username);
-
 }
