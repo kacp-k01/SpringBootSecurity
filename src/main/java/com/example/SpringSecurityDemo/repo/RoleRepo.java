a6732d70dbf0d1c3f2f9953b848978d9d4415361
Kacper <kacpkuc@gmail.com>
2023-11-11 12:46:01 +0100

    Old files removed

--- src/main/java/com/example/SpringSecurityDemo/repo/RoleRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.SpringSecurityDemo.repo;
-
-import com.example.SpringSecurityDemo.domain.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepo extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
--- src/main/java/com/example/SpringSecurityDemo/repo/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.SpringSecurityDemo.repo;
-
-import com.example.SpringSecurityDemo.domain.UserMember;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<UserMember, Long> {
-    UserMember findByUsername(String username);
-}
